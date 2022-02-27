faa43ef02d3c9a8f51a4b0f0fd6d2adb2da44805
BeckSM64 <becksm64@gmail.com>
2022-02-26 21:36:41 -0500

    :sparkles: download webm from most recent post and reupload to same text channel, for now

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import os
 import discord
 import datetime
 import re
+import urllib.request
+
 from os.path import join, dirname
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -14,8 +16,12 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# Setup bot
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Dictionary of guild ids to list of previously downloaded webms
+guild_id_to_webm_dict = {}
 
 @bot.event
 async def on_ready():
@@ -31,9 +37,29 @@ async def messageMonitorLoop():
         for channel in guild.text_channels:
 
             # Check most recent message for 4chan webm link
-            message = (await channel.fetch_message(channel.last_message_id))
-            if "https://i.4cdn.org" in message.content and ".webm" in message.content:
-                pass
+            if channel.last_message_id is not None:
+                print(channel.last_message_id)
+
+                # Try/catch to deal with the most recent message being deleted
+                try:
+                    message = (await channel.fetch_message(channel.last_message_id))
+                except:
+                    message = None
+                if ( message is not None) and ("https://i.4cdn.org" in message.content) and (".webm" in message.content):
+
+                    # Create url link
+                    url_link = message.content
+
+                    # Get filename to download from url link
+                    file_name = message.content.split('/')[-1]
+
+                    if guild_id_to_webm_dict.get(guild.id) != file_name:
+                        # Download webm from url
+                        urllib.request.urlretrieve(url_link, file_name)
+
+                        # Keep track of the file that was downloaded to avoid duplicates
+                        guild_id_to_webm_dict[guild.id] = file_name
+                        await channel.send(file=discord.File('./' + file_name))
 
 
 bot.run(TOKEN)
