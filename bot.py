5fa2c31eb1b700fa06856d5236410953ea186a2a
tomhanks <me@here.com>
2024-01-05 03:16:01 -0500

    removes 4chan webm archiver

--- bot.py
+++ bot.py
@@ -21,32 +21,6 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 guild_id_to_lists_of_webms_dict = {}
 
 
-def doesTextChannelExist(guild):
-
-    found_text_channel = False
-    for temp_channel in guild.text_channels:
-        if "webm-archive" in temp_channel.name:
-            found_text_channel = True
-            break
-    return found_text_channel
-
-
-def isFileNameInList(list_of_webms, file_name_to_compare):
-
-    # Return false if the list is empty
-    if list_of_webms is None:
-        return False
-
-    # Check if file name exists in list
-    found_file_name = False
-    for file_name in list_of_webms:
-        if file_name == file_name_to_compare:
-            found_file_name = True
-            break
-
-    return found_file_name
-
-
 def is_connected(ctx):
     """Returns True if bot is already connected to VC"""
     return discord.utils.get(bot.voice_clients, guild=ctx.guild)
@@ -129,81 +103,5 @@ async def playAudio(ctx, file_path, member_to_look_for):
                         await asyncio.sleep(1)
                     await vc.disconnect()
 
-
-def isMessageA4ChanWebm(message):
-    """Returns True if message contains a link to a 4chan webm"""
-    return ( message is not None) and (("https://i.4cdn.org" in message.content) or ("https://is2.4chan.org" in message.content)) and (".webm" in message.content)
-
-
-def getUrlFromMessage(message):
-    """Splits the message and returns the URL"""
-
-    # TODO: This won't work if a 4chan link is posted with no space
-    # between previous text and the link (ie. laksdjflaksdjhttps://i.4cdn.org )
-    message_content_array = message.content.split()
-    for split_message in message_content_array:
-        if ("https://i.4cdn.org" in split_message) or ("https://is2.4chan.org" in split_message):
-            url_link = split_message
-
-    return url_link
-
-
-async def createWebmArchiveChannel(message):
-    """Creates a webm archive channel if one doesn't already exist"""
-
-    # Check to see if webm-archive text channel exists
-    found_text_channel = doesTextChannelExist(message.guild)
-
-    if found_text_channel is False:
-        await message.guild.create_text_channel("webm-archive")
-
-
-def getTextChannelByName(message, text_channel_name):
-    """return text channel that matches name that was passed in"""
-    text_channel = discord.utils.get(message.guild.text_channels, name=text_channel_name)
-    return text_channel
-
-
-@bot.event
-async def on_message(message):
-
-    # required to process commmands when overriding on_message function
-    await bot.process_commands(message)
-
-    # check if message is a 4chan webm link
-    if isMessageA4ChanWebm(message):
-
-        # Create url link
-        url_link = getUrlFromMessage(message)
-
-        # Get filename to download from url link
-        file_name = url_link.split('/')[-1]
-
-        # List of webms that have already been archived
-        list_of_webms = guild_id_to_lists_of_webms_dict.get(message.guild.id)
-
-        # Check if newly posted webm was previously archived
-        if isFileNameInList(list_of_webms, file_name) is False:
-
-            # Create webm archive channel for server if it doesn't already exist
-            await createWebmArchiveChannel(message)
-
-            # Get archive channel
-            webm_archive_channel = getTextChannelByName(message, "webm-archive")
-
-            # Download webm from url
-            urllib.request.urlretrieve(url_link, file_name)
-
-            # Keep track of the file that was downloaded to avoid duplicates
-            if guild_id_to_lists_of_webms_dict.get(message.guild.id) is None:
-                guild_id_to_lists_of_webms_dict[message.guild.id] = [file_name]
-            else:
-                guild_id_to_lists_of_webms_dict[message.guild.id].append(file_name)
-
-            # Upload file to webm archive channel
-            await webm_archive_channel.send(file=discord.File('./' + file_name))
-
-            # Remove webm that was downloaded
-            os.remove(os.path.join("./", file_name))
 
 bot.run(TOKEN)
