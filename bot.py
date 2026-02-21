a640bbd4dc2406b269067ccf9bb41541829c110e
jonsaj <jonsaj@gmail.com>
2026-02-20 21:24:21 -0500

    adds the capability to download reddit videos. Does not work around discord 10MB limit

--- bot.py
+++ bot.py
@@ -90,8 +90,11 @@ def is_message_a_reddit_video_link(message):
             "skip_download": True,
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            url = get_url_from_message(message)
-            info = ydl.extract_info(url, download=False)
+            try:
+                url = get_url_from_message(message)
+                info = ydl.extract_info(url, download=False)
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
 
             if "entries" in info:
                 info = next(iter(info["entries"]), None)
@@ -231,6 +234,55 @@ def download_video_via_cloudscraper(url_link) -> bool:
     return result
 
 
+#downloaded_files
+def download_video_via_yt_dlp(url_link) -> (bool, str):
+    """
+    Handles the download attemp from reddit using yt_dlp
+
+    Args:
+        url_link: Link to the reddit video
+
+    Returns:
+        tuple (bool, str)
+        bool: Result of the download attempt
+        str: Downloaded filepath
+    """
+
+    #  This hook is given to yt_dlp so that it will provide us
+    # the filenames when it completes the downloads
+    # but because the final video and audio files still need
+    # to be merged, these filenames end up being less useful
+    # but the way that this hook works is too cool to delete.
+    # and, more importantly, for files that are downloaded
+    # but are too large for discord's 10mb limit, we may want
+    # to use these two files with ffmpeg directly to convert
+    # to webm. Probably won't though. Easier to run ffpmeg on
+    # the final .mp4, convert, and try the upload with that webm
+    #
+    # def hook(d):
+    #     if d['status'] == 'finished':
+    #         downloaded_files.append(d['filename'])
+
+    # These options are used to perform the reddit download
+    ydl_opts = {
+            'verbose': True,
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+            'merge_output_format': 'mp4', # this allows us to download a png
+            'noplaylist': True,
+            'http_headers': {'Accept': '*/*'}, # ai: Workaround for some Reddit URL issues
+            #'progress_hooks': [hook], #this hook allows us to find the intermediate files
+        }
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            # Download the video along with ydl info, which provides the filename
+            info = ydl.extract_info(url_link, download=True)
+            final_filename = info.get("filepath") or ydl.prepare_filename(info)
+            print("final filename: " + final_filename)
+            return (True, final_filename)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    return (False, "")
+
 @bot.event
 async def on_message(message):
     """
@@ -286,6 +338,49 @@ async def on_message(message):
 
     if is_message_a_reddit_video_link(message):
         print(message.content + " is a video from reddit")
+
+        # Create url link
+        #   removing trailing slash to make it easier
+        #   to split for the 'already archived' dict
+        url_link = get_url_from_message(message).rstrip('/')
+
+        #  Get media name to download from url link
+        # this name is not the final filename, but we will use
+        # it for the list_of_webmbs_dict. The final filename
+        # will not be known until the download is complete
+        url_psudo_name = url_link.split('/')[-2] + '/' + url_link.split('/')[-1]
+        print("will retrieve video from" + url_psudo_name)
+
+        # List of webms that have already been archived
+        list_of_webms = guild_id_to_lists_of_webms_dict.get(message.guild.id)
+
+        # Check if newly posted webm was previously archived
+        if is_file_name_in_list(list_of_webms, url_psudo_name) is False:
+
+            # Create webm archive channel for server if it doesn't already exist
+            await create_webm_archive_channel(message)
+
+            # Get archive channel
+            webm_archive_channel = get_text_channel_by_name(message, "webm-archive")
+
+            # Attempt to download the video
+            # result contains (bool, filename)
+            result = download_video_via_yt_dlp(url_link)
+
+            # Only keep track of the file if it downloaded successfully
+            if result[0]:
+                # Keep track of the file that was downloaded to avoid duplicates
+                if guild_id_to_lists_of_webms_dict.get(message.guild.id) is None:
+                    guild_id_to_lists_of_webms_dict[message.guild.id] = [url_psudo_name]
+                else:
+                    guild_id_to_lists_of_webms_dict[message.guild.id].append(url_psudo_name)
+
+                # Upload file to webm archive channel
+                file_name = result[1]
+                await webm_archive_channel.send(file=discord.File('./' + file_name))
+
+                # Remove webm that was downloaded
+                os.remove(os.path.join("./", file_name))
 
 
 bot.run(TOKEN)
